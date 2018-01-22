da644c82dd7d37b4ad5aa198487121dc30fb4c10
Marcelo Montovani Thomaz <marcelomthomaz@gmail.com>
2018-01-22 22:51:35 +0000

    basic auth working

--- client/components/Login.js
+++ client/components/Login.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 import TextField from 'material-ui/TextField'
-import IconButton from 'material-ui/IconButton';
-import Input, { InputLabel, InputAdornment  } from 'material-ui/Input';
-import { FormControl, FormHelperText, FormControlLabel  } from 'material-ui/Form';
-import Checkbox from 'material-ui/Checkbox';
-import Visibility from 'material-ui-icons/Visibility';
-import VisibilityOff from 'material-ui-icons/VisibilityOff';
+import IconButton from 'material-ui/IconButton'
+import Input, { InputLabel, InputAdornment  } from 'material-ui/Input'
+import { FormControl, FormHelperText, FormControlLabel  } from 'material-ui/Form'
+import Checkbox from 'material-ui/Checkbox'
+import Visibility from 'material-ui-icons/Visibility'
+import VisibilityOff from 'material-ui-icons/VisibilityOff'
+import Button from 'material-ui/Button'
 import { withStyles } from 'material-ui/styles'
+
+import { Redirect } from 'react-router-dom'
 
 const styles = {
    form_wrapper: {
@@ -27,7 +30,8 @@ class Login extends Component {
       showPassword: false,
       password: '',
       name: '',
-      rememberMe: false
+      rememberMe: false,
+      redirectToReferrer: false
    }
 
    handleChange = prop => event => {
@@ -45,12 +49,38 @@ class Login extends Component {
    handleMouseDownPassword = event => {
       event.preventDefault();
 
-   };
+   }
+
+   handleSubmit = async (e) => {
+      e.preventDefault()
+
+      const res = await fetch('/login', {
+         method: 'POST',
+         headers: {
+            'Content-type':'application/json',
+            'Accept':'application/json'
+         },
+         body: JSON.stringify({email: this.state.email, password: this.state.password})
+      })
+
+      const data = await res.json()
+
+      if (data.ok) {
+         this.setState({token: data.token, name: data.name, redirectToReferrer: true})
+      }
+   }
 
    render() {
       const { classes } = this.props
+      const { redirectToReferrer  } = this.state
 
-      return (
+         if (redirectToReferrer) {
+            return (
+               <Redirect to="/"/>
+               )
+         }
+
+         return (
          <div className={classes.form_wrapper}>
             <form id="signin" autoComplete="off" className={classes.form_class}>
                <FormControl>
@@ -91,6 +121,7 @@ class Login extends Component {
                      label='Remember me'
                   />
                </FormControl>
+               <Button raised color="primary" type="submit" onClick={this.handleSubmit}>Log in</Button>
             </form>
             <a href="/signup">Sign up</a>
             <a href="/recover">Forgot your password?</a>
